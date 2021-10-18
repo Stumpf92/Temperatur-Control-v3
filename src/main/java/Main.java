b5669bafcefd2298b3c09ef88c49df82ace175bc
Stumpf92 <90602289+Stumpf92@users.noreply.github.com>
2021-10-18 13:20:47 +0200

    20211018

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("scheiÃŸe alles gelÃ¶scht 1");
-		System.out.println("junge richtig scheiÃŸe und so");
-		System.out.println("jetzt aber richtig 18.okt.2021");
-		System.out.println("check check, zuhause arbeiten und so");
-		System.out.println("geÃ¤ndert in github, jetzt schonwieder");
-		System.out.println("so und dann was im bueeeroooooogenändert");
-		System.out.println("so und nochmal zum test junge");
+		System.out.println("so jetzt l�ufts doch oder");
+
 	}
 
 }
