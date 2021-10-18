fbc3aa845c3ed6a99d3cd4e7cb454dfeecb37c9d
Stumpf92 <90602289+Stumpf92@users.noreply.github.com>
2021-10-18 13:18:25 +0200

    20211018

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("jetzt aber richtig 18.okt.2021");
 		System.out.println("check check, zuhause arbeiten und so");
 		System.out.println("ge√§ndert in github, jetzt schonwieder");
+		System.out.println("so und dann was im bueeeroooooogen‰ndert");
 	}
 
 }
